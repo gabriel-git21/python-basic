002a3dd07c24eb723cec774aee8e95815973a9e7
Joao Gabriel <gabryelschmitz6@gmail.com>
2026-02-18 01:53:58 -0300

    implementamos aaaaaaaaaaaaaa

--- hello.py
+++ hello.py
@@ -1,2 +1,39 @@
-print("Hello, World")
+#!/usr/bin/env python3
+"""Hello World Multi Linguas.
 
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+ correspondente.
+
+ Usage:
+
+ Depends on Variable LANG configured correctly ex:
+
+    export LANG=pt_BR
+
+Execution:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "JAODASJAPA"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
+
+
+print(msg)
